e2aada212ba2054a3622cf985206a697e5acdef7
Miguel Contreras <mcontrerasjaen@gmail.com>
2026-01-16 11:16:42 +0000

    generador de excusas primera parte

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let excusas = document.getElementById("excusas")
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  //console.log("Hello Rigo from the console!");
+
+  let excusa = who[Math.floor(Math.random()* who.length)]
+  excusa += " " + action[Math.floor(Math.random()* action.length)] 
+  excusa += " " + what[Math.floor(Math.random()* what.length)]
+  excusa += " " + when[Math.floor(Math.random()* when.length)]
+
+  excusas.innerHTML = excusa
+  
 };
